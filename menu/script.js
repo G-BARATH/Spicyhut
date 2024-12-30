1f69e5200274c9a14115f04381d5ed1a34a9c8b8
barathg <barathg>
2024-12-30 06:58:50 -0800

    minus

--- menu/script.js
+++ menu/script.js
@@ -1,11 +1,11 @@
 const foods = [
-  { name: "Vegetarian Pizza  &#8377-350", category: "Vegetarian", description: "Delicious vegetarian pizza with fresh toppings.", image:"images/vegepizza.jpg" },
-  { name: "Chicken Biryani   &#8377-400", category: "Non-Vegetarian", description: "Spicy and flavorful chicken biryani.", image:"images/chickenbriyani.jpg" },
-  { name: "Chocolate Cake  &#8377-700", category: "Desserts", description: "Rich chocolate cake with creamy icing.", image: "images/choccake.jpg" },
-  { name: "Vegetable Burger  &#8377-300", category: "Vegetarian", description: "Healthy vegetable burger with a crunchy patty.", image: "images/vegeburger.jpg" },
-  { name: "Fish Curry  &#8377-400", category: "Non-Vegetarian", description: "Tasty fish curry with spices.", image: "images/fishcury.jpg" },
-  { name: "Ice Cream  &#8377-200", category: "Desserts", description: "Cool and creamy vanilla ice cream.", image: "images/icecream.jpg" },
-  { name: "Mutton Briyani  &#8377-600", category: "Non-Vegetarian", description: "Spicy and flavorful mutton briyani.", image: "images/muttonbriyani.jpg" },
+  { name: "Vegetarian Pizza  &#8377 350", category: "Vegetarian", description: "Delicious vegetarian pizza with fresh toppings.", image:"images/vegepizza.jpg" },
+  { name: "Chicken Biryani   &#8377 400", category: "Non-Vegetarian", description: "Spicy and flavorful chicken biryani.", image:"images/chickenbriyani.jpg" },
+  { name: "Chocolate Cake  &#8377 700", category: "Desserts", description: "Rich chocolate cake with creamy icing.", image: "images/choccake.jpg" },
+  { name: "Vegetable Burger  &#8377 300", category: "Vegetarian", description: "Healthy vegetable burger with a crunchy patty.", image: "images/vegeburger.jpg" },
+  { name: "Fish Curry  &#8377 400", category: "Non-Vegetarian", description: "Tasty fish curry with spices.", image: "images/fishcury.jpg" },
+  { name: "Ice Cream  &#8377 200", category: "Desserts", description: "Cool and creamy vanilla ice cream.", image: "images/icecream.jpg" },
+  { name: "Mutton Briyani  &#8377 600", category: "Non-Vegetarian", description: "Spicy and flavorful mutton briyani.", image: "images/muttonbriyani.jpg" },
 ];
 
 let filteredFoods = foods;
